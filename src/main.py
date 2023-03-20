58e85e0ba9b92f8eb89070402beb71dd67d1f4c7
kolynzb <collinsbenda360@gmail.com>
2023-03-20 13:35:29 +0300

    refactor: add entry point for application

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+"""
+
+@author: kolynzb
+"""
+from gui.app import main 
+
+# from src.backend.scripts import  dl_video
+
+# dl_audio.download_audio("https://youtu.be/up7e41593fY")
+
+# dl_video.download_playlist("https://youtu.be/up7e41593fY")
