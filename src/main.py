0d3f501f7d1c9d5b3a6ff9833ddc56efe3b8cf78
kolynzb <collinsbenda360@gmail.com>
2023-03-22 19:38:46 +0300

    feat: restore main

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@
 
 @author: kolynzb
 """
-from gui.app import main 
+from gui.app import run_gui
 
 # from src.backend.scripts import  dl_video
 
 # dl_audio.download_audio("https://youtu.be/up7e41593fY")
 
 # dl_video.download_playlist("https://youtu.be/up7e41593fY")
+
+run_gui()
