748f649ca41e0d93b35fe2f10a1731c09058bff3
kolynzb <collinsbenda360@gmail.com>
2023-03-22 17:44:21 +0300

    feat: add custom text to button

--- src/gui/widgets/buttons.py
+++ src/gui/widgets/buttons.py
@@ -7,6 +7,25 @@ class OptBtn(ft.UserControl):
         self.btn_name = btn_name
         self.onclick = onclick
 
+
     def build(self):
-        opt_btn= ft.TextButton(text=self.btn_name,on_click=self.onclick)
+
+        opt_btn= ft.OutlinedButton(
+            width=150,
+            content=ft.Row(
+                [
+                    ft.Text(value=self.btn_name,style=ft.TextThemeStyle.LABEL_MEDIUM,font_family="Mont",weight=ft.FontWeight.W_400),
+                ],
+                alignment=ft.MainAxisAlignment.SPACE_AROUND,
+            ),
+            tooltip=f"Download {self.btn_name}",
+            style=ft.ButtonStyle(
+            color={
+                ft.MaterialState.HOVERED: ft.colors.WHITE,
+                ft.MaterialState.FOCUSED: ft.colors.BLUE,
+                ft.MaterialState.DEFAULT: ft.colors.BLACK,
+            }
+            )
+        )
+
         return opt_btn
